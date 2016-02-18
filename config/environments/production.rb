de96cb2af472a6af8f7d96e3223809448733caff
TheNotary <no@email.plz>
2016-02-18 18:40:43 +0000

    switched to using application.yml rather than secrets.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,14 +74,14 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
-    domain: Rails.application.secrets.domain_name,
+    domain: ENV['domain_name'],
     authentication: "plain",
     enable_starttls_auto: true,
-    user_name: Rails.application.secrets.email_provider_username,
-    password: Rails.application.secrets.email_provider_password
+    user_name: ENV['email_provider_username'],
+    password: ENV['email_provider_password']
   }
   # ActionMailer Config
-  config.action_mailer.default_url_options = { :host => Rails.application.secrets.domain_name }
+  config.action_mailer.default_url_options = { :host => ENV['domain_name'] }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
