6c131172bc479b428004d13e072c1aa761990f39
vivian <vivian@eff.org>
2016-03-14 16:34:34 -0700

    Log users in based on basic auth.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,19 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_action :login_by_basic_auth
+
   def admin_only
     unless current_user.try(:admin?) || current_user.try(:admin_user_mode?)
       redirect_to '/', :alert => "Access denied."
+    end
+  end
+
+  def login_by_basic_auth
+    if ActionController::HttpAuthentication::Basic::has_basic_credentials?(request)
+      name = ActionController::HttpAuthentication::Basic::user_name_and_password(request)[0]
+      user = User.where(email: name + "@eff.org")[0]
+      sign_in :user, user if user
     end
   end
 
