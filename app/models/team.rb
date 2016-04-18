285ce24e1a8a2a22b593330f3c30b05688750620
TheNotary <no@email.plz>
2016-04-18 16:18:49 -0700

    Optimized Team.get_runners with elite sql query majix

--- app/models/team.rb
+++ app/models/team.rb
@@ -17,11 +17,40 @@ class Team < ActiveRecord::Base
     Team.new(name: "Team Runner Up", weight: 200)
   end
 
-  # This function is soooo EFFing wasteful! Only use it in mailer views
-  # The users#index page iteratively builds the runners array as it generates
-  # the view to avoid needing a second query
   def self.get_runners
-    User.all.select { |u| u.upcoming_fab.id.nil? }
+    # User.all.select { |u| u.upcoming_fab.id.nil? }
+    target_period = Fab.get_start_of_current_fab_period
+    p1 = target_period.strftime("%Y-%m-%d")
+    p2 = (target_period + 1.day).strftime("%Y-%m-%d")
+
+    # query_non_runners = <<-EOT.strip_heredoc
+    #
+    # SELECT "users"."name", "fabs"."period"
+    # FROM "users"
+    #   INNER JOIN "fabs"
+    #   ON "fabs"."user_id" = "users"."id"
+    #     WHERE "fabs"."period" BETWEEN date('#{p1}') AND date('#{p2}');
+    #
+    # EOT
+
+    q = <<-EOT.strip_heredoc
+
+    SELECT *
+    FROM "users"
+      INNER JOIN "fabs"
+      ON "fabs"."user_id" = "users"."id"
+      GROUP BY "users"."email"
+        HAVING max(
+          case
+            WHEN "fabs"."period" BETWEEN date('#{p1}') AND date('#{p2}') THEN
+              1
+            ELSE
+              0
+            END
+        ) = 0
+    EOT
+
+    User.find_by_sql(q)
   end
 
 end
