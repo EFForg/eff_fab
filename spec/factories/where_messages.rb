eb6cd42f5b660e073417feff2272b0c792ab7abd
K. Stewart <ignitiant@gmail.com>
2018-03-14 14:35:27 -0700

    Better tests with random data

--- spec/factories/where_messages.rb
+++ spec/factories/where_messages.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :where_message do
     association :user
-    body %w(wfh auw eom flex wfl pto vacation am pm sick sl).sample(3).join(' ')
+    body { Faker::Coffee.notes }
     sent_at 5.minutes.ago
   end
 end
