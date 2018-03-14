ad822d7f20f4ebcdebbf0a73fac52e51ac486e10
K. Stewart <ignitiant@gmail.com>
2018-03-14 11:57:06 -0700

    Where factory: create random bodies

--- spec/factories/where_messages.rb
+++ spec/factories/where_messages.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :where_message do
     association :user
-    body "wfh auw eom"
+    body %w(wfh auw eom flex wfl pto vacation am pm sick sl).sample(3).join(' ')
     sent_at 5.minutes.ago
   end
 end
