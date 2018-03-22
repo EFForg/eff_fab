a7ecdc96b65305ad1df331d98f9f286a49e180da
K. Stewart <ignitiant@gmail.com>
2018-03-22 15:01:50 -0700

    Better where message bodies

--- lib/wherebot.rb
+++ lib/wherebot.rb
@@ -1,4 +1,5 @@
 require 'net/imap'
+require 'mail'
 
 class Wherebot
   TRASH = "Trash".freeze
@@ -38,39 +39,92 @@ class Wherebot
   end
 
   class Message
-    def initialize(imap, message_id, destroy)
+    def initialize(imap, email_id, destroy = false)
       @imap = imap
-      @id = message_id
+      @id = email_id
       @destroy = destroy
     end
 
     def create
       WhereMessage.find_or_create_by(
         provenance: 'where@eff.org',
-        sent_at: DateTime.parse(message.date),
-        body: body_for(message),
+        sent_at: DateTime.parse(headers.date),
+        body: body,
         user: User.find_by(
-          email: "#{message.from[0].mailbox}@#{message.from[0].host}"
+          email: "#{headers.from[0].mailbox}@#{headers.from[0].host}"
         )
       )
 
       destroy_message
     end
 
-    def message
-     @message ||= @imap.fetch(@id, "ENVELOPE")[0].attr["ENVELOPE"]
+    def body
+      body = get_text
+      body = strip_quotes_and_signatures_from(body)
+      body = reject_encryption(body)
+      strip_junk_from(body)
+      body = remove_trailing_junk(body)
+      "#{nice_subject}\n#{body}".strip.chomp.force_encoding('UTF-8')
     end
 
-    def body_for(message)
-      message.subject.gsub!('[EFF-where] ', '')
+    private
 
-      body = if (subject = message.subject).split(' ').last.downcase == 'eom'
-        subject
-      else
-        "#{subject}\n#{@imap.fetch(@id, 'BODY[TEXT]')[0].attr['BODY[TEXT]']}"
+    def headers
+      @headers ||= @imap.fetch(@id, "ENVELOPE")[0].attr["ENVELOPE"]
+    end
+
+    def full_body
+      @full_body ||= @imap.fetch(@id, 'BODY[TEXT]')[0].attr['BODY[TEXT]']
+    end
+
+    def get_text
+      body = full_body.split("Content-Type: text/")[1] || full_body
+      body = body.split("Content-Transfer-Encoding: ")[1] || body
+
+      if body.first(4) == "html"
+        body = ActionController::Base.helpers.strip_tags(body) || body
       end
 
-      body.force_encoding('UTF-8')
+      body
+    end
+
+    def strip_quotes_and_signatures_from(body)
+      has_eom = body.match(/.*eom.?/i)
+      return has_eom[0] if has_eom.present?
+      body = body.split("Sent from").first
+      body = body.split("\nOn").first
+      body = body.split('--').first
+      body = body.split(/Content-Language: en-../).last
+      body = body.split('********').first
+    end
+
+    def reject_encryption(body)
+      # this message is either made up only of encryption,
+      # or is insufferably sesquepedalian.
+      body = '' if body.present? && body.split(' ').map(&:length).min > 10
+      body
+    end
+
+    def strip_junk_from(body)
+      return unless body.present?
+      body.gsub!(/\n|\r/, ' ')
+      body.gsub!('  ', ' ')
+      body.remove!(/quoted-printable|\r\n|\n\r|=2E|7bit|html;|charset=utf-8/)
+      body.strip!
+    end
+
+    def remove_trailing_junk(body)
+      if %w(= % &).include?(body.last)
+        body[0..-2]
+      else
+        body
+      end
+    end
+
+    def nice_subject
+      subject = headers.subject
+      subject.gsub!('[EFF-where] ', '')
+      subject
     end
 
     def destroy_message
--- spec/lib/wherebot_spec.rb
+++ spec/lib/wherebot_spec.rb
@@ -89,4 +89,122 @@ RSpec.describe Wherebot do
       end
     end
   end
+
+  describe 'Wherebot::Message.body' do
+    let(:wherebot_message) { Wherebot::Message.new(double(:imap), rand(1..100)) }
+    let(:body) { wherebot_message.body }
+
+    before do
+      allow(wherebot_message).to receive(:full_body).and_return(message)
+      allow(wherebot_message).to receive(:headers).and_return(double(subject: ""))
+    end
+
+    context "when message is all dumb whitespace" do
+      let(:message) { "\r\n\r\n\r\n" }
+      specify { expect(body).to be_empty }
+    end
+
+    context "when message is just whitespace and signature" do
+      let(:message) { "\r\n\r\nSent from my iPhone\r\n\r\n\r\n" }
+      specify { expect(body).to be_empty }
+    end
+
+    context "when message is just signature" do
+      let(:message) do
+        "\r\n-- \r\nNamey <namey@namey.org>\r\Staff Technologist https://www.namey.org/\r\nElectronic Frontier Foundation https://www.namey.org/join\r\n815 Eddy\r\n\r\n\r\n"
+      end
+      specify { expect(body).to be_empty }
+    end
+
+    context "when message is just a PGP key" do
+      let(:message) do # this is no one's actual PGP key, just a mess of characters
+        "PGRpdiBkaTotallyZGlzY2xvc3VyZTo\r\naGUgY29ySecuregdGhlIHB\r\nYmVsX3A9Noise8BfYnV0d==\r\n\r\n\r\n\r\n"
+      end
+      specify { expect(body).to be_empty }
+    end
+
+    context "when message is in MIME format" do
+      let(:message) do
+        "This is a multi-part message in MIME format.\r\n--------------2709SecureNoise\r\nContent-Type: text/plain; charset=utf-8\r\nContent-Transfer-Encoding: 7bit\r\n\r\Preparing for the uprising, then restoring my depleted energy reserves. Back Tuesday!\r\n\r\n-- \r\Namey\r\nGrant Writer\r\nElectronic Frontier Foundation\r\n\r\n\r\n--------------kajsdk89KLJsecurityNoiselklLK\r\nContent-Type: text/html; charset=utf-8\r\nContent-Transfer-Encoding: 7bit\r\n\r\n<html>\r\n  <head>\r\n\r\n    <meta http-equiv=\"content-type\" content=\"text/html; charset=utf-8\">\r\n  </head>\r\n  <body text=\"#000000\" bgcolor=\"#FFFFFF\">\r\n    <p>Preparing for the uprising, then restoring my depleted energy reserves. Back\r\n      Tuesday!<br>\r\n    </p>\r\n    <pre class=\"moz-signature\" cols=\"72\">-- \r\Namey\r\nGrant Writer\r\nElectronic Frontier Foundation\r\n\r\n</pre>\r\n  </body>\r\n</html>\r\n\r\n--------------LKJLJKKJsecurityNoise89908LKJL--\r\n\r\n\r\n"
+      end
+
+      it "returns the important part" do
+        expect(body).to eq(
+          "Preparing for the uprising, then restoring my depleted energy reserves. Back Tuesday!"
+        )
+      end
+    end
+
+    context "when message is in OpenPGP/MIME format" do
+      let(:message) do
+        "This is an OpenPGP/MIME signed message (RFC 4880 and 3156)\r\n--\r\nContent-Type: multipart/mixed; boundary=\"\";\r\n protected-headers=\"v1\"\r\nFrom: Namey <namey@eff.org>\r\nTo: \"where@namey.org\" <where@namey.org>\r\nMessage-ID: <ba5@namey.org>\r\nSubject: Namey omw! eta 11:30\r\n\r\n--\r\nContent-Type: text/plain; charset=utf-8\r\nContent-Language: en-US\r\nContent-Transfer-Encoding: quoted-printable\r\n\r\nI jumped in a ball pit and then totally slept through my alarm, oh noes!=\r\n=2E\r\n--=20\rNamey\r\nTechnologist General, Electronic Frontier Foundation\r\nGPG: https://www.namey.org/files/key.txt\r\n\r\n\r\n--890987SecureNoise989087--\r\n\r\n\nContent-Type: application/pgp-signature; name=\"signature.asc\"\r\nContent-Description: OpenPGP digital signature\r\nContent-Disposition: attachment; filename=\"signature.asc\"\r\n\r\n-----BEGIN PGP SIGNATURE-----\r\n\LKLKJLKLK/0989898798++\r\r\r\n=V4eA\r\n-----END PGP SIGNATURE-----\r\n\r\n----\r\n\r\n\r\n"
+      end
+
+      it "returns the important part" do
+        expect(body).to eq(
+          "I jumped in a ball pit and then totally slept through my alarm, oh noes!"
+        )
+      end
+    end
+
+    context "when message is from K9 mail" do
+      let(:message) do
+        "------slkj8978KLJLsecurityNoise\r\nContent-Type: text/plain;\r\n charset=utf-8\r\nContent-Transfer-Encoding: quoted-printable\r\n\r\nShould be back in the office within an hour\r\n--=20\r\nSent from my Android device with K-9 Mail=2E wherePlease excuse my brevity=\r\n=2E\r\n------LKJ987SecurityNoise\r\nContent-Type: text/html;\r\n charset=utf-8\r\nContent-Transfer-Encoding: quoted-printable\r\n\r\nShould be back in the office within an hour<br>\r\n-- <br>\r\nSent from my Android device with K-9 Mail=2E wherePlease excuse my brevity=\r\n=2E\r\n------SLKJ987KLJSecurityNoise--\r\n\r\n\r\n"
+      end
+      it "returns only the good stuff" do
+        expect(body).to eq(
+          "Should be back in the office within an hour"
+        )
+      end
+    end
+
+    context "when message is only HTML" do
+      let(:message) do
+        "Content-Type: text/html;\r\n\tcharset=utf-8<html><head><meta http-equiv=3D\"Content-Type\" content=3D\"text/html; =\r\ncharset=3Dutf-8\"></head><body style=3D\"word-wrap: break-word; =\r\n-webkit-nbsp-mode: space; line-break: after-white-space;\" class=3D\"\"><div class=3D\"\"><br class=3D\"\"></div><div class=3D\"\">I will be OOO all next week.</div><div class=3D\"\">"
+      end
+
+      it "returns only the good stuff" do
+        expect(body).to eq(
+          "I will be OOO all next week."
+        )
+      end
+    end
+
+    context "when message is mostly fine, but has whitespace crud," do
+      let(:message) { "HTTPSing Everywhere.  AUW.\r\n\r\n\r\n" }
+      it "strips off the whitespace" do
+        expect(body).to eq("HTTPSing Everywhere. AUW.")
+      end
+    end
+
+    context "when message replies to another message" do
+      let(:message) do
+        "leaving early.\r\n\r\n\r\nOn 3/21/18 4:52 PM, Namey wrote:\r\n>\r\n>\r\n\r\n\r\n\r\n"
+      end
+      it "returns only the new content" do
+        expect(body).to eq("leaving early.")
+      end
+
+      context "with inline quotes" do
+        let(:message) do
+          "Re: Namey WFH today\n\r\nContent-Language: en-USoffline for a bit Thanks,********************************\r\nNamey\r\nCool Coordinator\r\nElectronic Frontier Foundation (EFF)\r\nPGP Fingerprint: S0M3 LTR5"
+        end
+        it "returns only the new content" do
+          expect(body).to eq("offline for a bit Thanks,")
+        end
+      end
+    end
+
+    context  do
+      #I don't know why, but this message was failing in a different way
+      let(:message) do
+        "Keep getting stuck in peanut butter. I think I'm just very\r\ntired.\r\n\r\n-- \r\nNamey\r\nPeanut Butter Coordinator\r\n815 Eddy Street\r\nSan Francisco, CA\r\n\r\nnamey.org/join\r\n\r\n\r\n\r\n"
+      end
+      it "returns only the good stuff" do
+        expect(body).to eq(
+          "Keep getting stuck in peanut butter. I think I'm just very tired."
+        )
+      end
+    end
+  end
 end
