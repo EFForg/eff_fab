1318ac2af90ade33f68b0f7736b6dc8931d84f39
K. Stewart <ignitiant@gmail.com>
2018-03-29 18:29:41 -0700

    Sort where list by user name

--- app/controllers/wheres_controller.rb
+++ app/controllers/wheres_controller.rb
@@ -2,7 +2,7 @@ class WheresController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @users = User.all
+    @users = User.all.order("name asc")
   end
 
   def user_list
