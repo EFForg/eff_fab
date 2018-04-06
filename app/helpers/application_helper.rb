466e438d2a58d5a3aa44bc6e5d0ec08fd43ffe04
Kai Stewart <ignitiant@gmail.com>
2018-04-05 19:04:27 -0700

    Small changes to the Whereabouts helpers
    Per @wioux's insightful suggestions.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -33,8 +33,7 @@ module ApplicationHelper
     return nil unless time
 
     formatted_time = time.in_time_zone("Pacific Time (US & Canada)")
-      .strftime("%a, %b %e, %Y %l:%M %p %Z")
-      .gsub("  ", " ")
+      .strftime("%a, %b %e, %Y %-l:%M %p %Z")
     time_ago = distance_of_time_in_words(time, Time.current)
     "#{time_ago} ago (#{formatted_time})"
   end
--- app/helpers/where_helper.rb
+++ app/helpers/where_helper.rb
@@ -1,10 +1,9 @@
 module WhereHelper
   def where_or_stub_for(user)
-    user.last_whereabouts ||
-      OpenStruct.new(body: I18n.t('shruggie'), provenance: nil, sent_at: nil)
+    user.last_whereabouts || WhereMessage.new(body: I18n.t('shruggie'))
   end
 
   def reply_subject(where)
-    "Re: #{where.body.split("\n").first || where.body}"
+    "Re: #{where.body.split("\n").first}"
   end
 end
