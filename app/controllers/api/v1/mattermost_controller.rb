6670d3dbcdf09effab4e0208f0d9dade1a81e6c6
Kai Stewart <ignitiant@gmail.com>
2018-04-09 14:34:47 -0700

    Mattermost commands shouldn't look for basic auth

--- app/controllers/api/v1/mattermost_controller.rb
+++ app/controllers/api/v1/mattermost_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::MattermostController < Api::ApplicationController
   require 'commands'
 
+  skip_before_action :login_by_basic_auth
   before_action :authenticate_mattermost
 
   def where
