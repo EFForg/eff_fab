bf761eeeceb69d005f41a137848af6f9f1b2b5b5
Kai Stewart <ignitiant@gmail.com>
2018-04-20 12:44:43 -0700

    Remove Mattermost IP check
    The way the loadbalancer and firewalls are set up, it doesn't make sense
    to check an IP.

--- app/controllers/api/v1/mattermost_controller.rb
+++ app/controllers/api/v1/mattermost_controller.rb
@@ -2,7 +2,6 @@ class Api::V1::MattermostController < Api::ApplicationController
   require_dependency 'commands'
 
   skip_before_action :login_by_basic_auth
-  before_action :authenticate_mattermost
 
   def where
     render json: Commands::Where.new(command_params).response
@@ -13,10 +12,6 @@ class Api::V1::MattermostController < Api::ApplicationController
   end
 
   private
-
-  def authenticate_mattermost
-    head :unauthorized unless ENV['MATTERMOST_IPS'].split.include?(request.remote_ip)
-  end
 
   def command_params
     params.permit(:user_name, :text, :token)
