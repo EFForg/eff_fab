629f2666518a224057d53bd0969288e02442c042
Kai Stewart <ignitiant@gmail.com>
2018-05-18 15:27:04 -0700

    Actually save custom attrs in the fab factories

--- spec/factories/fabs.rb
+++ spec/factories/fabs.rb
@@ -4,17 +4,21 @@ FactoryGirl.define do
   end
 
   factory :fab_due_in_prior_period, parent: :fab do
-    after(:create) do |fab|
+    before(:create) do |fab|
       fab.period = (Fab.get_start_of_current_fab_period - 7.days)
       fab.created_at = fab.period
+    end
+    after(:create) do |fab|
       fab.backward.first.update_attributes(body: "I have an old note")
     end
   end
 
   factory :fab_due_in_current_period, parent: :fab do
-    after(:create) do |fab|
+    before(:create) do |fab|
       fab.period = (Fab.get_start_of_current_fab_period)
       fab.created_at = fab.period
+    end
+    after(:create) do |fab|
       fab.backward.first.update_attributes(body: "I have a note")
     end
   end
